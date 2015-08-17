ca2e3a6b9cff23b9fa33c79c0048f4ed1eaa9bd6
listlfa <list_lfa@yahoo.com>
2015-08-18 07:47:18 +1000

    a

---
+++ manifests/init.pp
@@ -0,0 +1,22 @@
+#Example - Create a new forest
+    class {'windows_ad':
+      install                => present,
+      installmanagementtools => true,
+      restart                => true,
+      installflag            => true,
+      configure              => present,
+      configureflag          => true,
+      domain                 => 'forest',
+      domainname             => 'jre.local',
+      netbiosdomainname      => 'jre',
+      domainlevel            => '6',
+      forestlevel            => '6',
+      databasepath           => 'c:\\windows\\ntds',
+      logpath                => 'c:\\windows\\ntds',
+      sysvolpath             => 'c:\\windows\\sysvol',
+      installtype            => 'domain',
+      dsrmpassword           => 'password',
+      installdns             => 'yes',
+      localadminpassword     => 'password',
+    }
+
